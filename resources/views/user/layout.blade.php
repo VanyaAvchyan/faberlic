7c9493154a8e302327897a5a351bc59cc1f980c8
VanyaAVchyan <vanyaavchyan@gmail.com>
2018-04-01 23:36:03 +0400

    5.1.10 add training link

--- resources/views/user/layout.blade.php
+++ resources/views/user/layout.blade.php
@@ -104,6 +104,7 @@
                       <span class=" fa fa-angle-down"></span>
                     </a>
                     <ul class="dropdown-menu dropdown-usermenu pull-right">
+                      <li><a href="{{url('training/videos')}}">Training 1</a></li>
                       <li><a href="{{url('user/logout')}}"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
                     </ul>
                   </li>
