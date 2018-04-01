878d41e4cac393a0fd00a577899d0072efe6d96d
VanyaAVchyan <vanyaavchyan@gmail.com>
2018-04-01 23:23:20 +0400

    5.1.9 training videos style

--- resources/views/trainings/level.blade.php
+++ resources/views/trainings/level.blade.php
@@ -1,17 +1,16 @@
 @extends('site/layout')
 @section('content')
-<div class="promo-block">
-    <div class="container">
-        <div class="col-sm-6">
-            <div class="promo-block-img-wrap">
-                <h3>Training videos</h3>
-                @foreach($training_videos as $training_video)
-                    <div class="col-md-3">
-                        {!! $training_video->{'url_'.$lang} !!}
-                    </div>
-                @endforeach
-            </div>
-        </div>
+<div class="container content-lg">
+    <div class="col-lg-12">
+        <h2>Training videos</h2>
+        @foreach($training_videos as $training_video)
+            {!! $training_video->{'url_'.$lang} !!}
+        @endforeach
     </div>
 </div>
+<style>
+    p,span {
+        padding: 10px
+    }
+</style>
 @endsection
