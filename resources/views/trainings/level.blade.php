05c36efcecf32b4d003b22605b60ff9ca5c400d1
VanyaAVchyan <vanyaavchyan@gmail.com>
2018-04-06 20:53:54 +0400

    5.1.15

--- resources/views/trainings/level.blade.php
+++ resources/views/trainings/level.blade.php
@@ -4,7 +4,9 @@
     <div class="col-lg-12">
         <h2>Training videos</h2>
         @foreach($training_videos as $training_video)
+        <div class="col-lg-2">
             {!! $training_video->{'url_'.$lang} !!}
+        </div>
         @endforeach
     </div>
 </div>
